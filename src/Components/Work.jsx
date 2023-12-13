96440f3495e474a0c2591e52fe29018ada0a88bd
mrcoder57 <tiwariji2300@gmail.com>
2023-12-13 09:21:10 +0530

    fix enddates

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const getRandomColor = () => {
   const colors = ["#FF0000", "#00FF00", "#0000FF", "#FFA500", "#800080"]; // Add more colors if needed
@@ -28,37 +30,52 @@ const Work = () => {
 
   return (
     <div className=" font-mono bg-slate-100 mt-28 shadow-lg ">
-      <h1 className=" my-3 text-center text-3xl font-bold text-[#d8874a]">My Experties</h1>
-      <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical mx-8 text-slate-700">
-        {data.map((exp, index) => (
-          <li key={index} className={index % 2 === 0 ? "timeline-start" : "timeline-end"}>
-            <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
+      
+        <h1 className=" my-3 text-center text-3xl font-bold text-[#d8874a]">
+          My Experties
+        </h1>
+        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical mx-8 text-slate-700">
+          {data.map((exp, index) => (
+            <li
+              key={index}
+              className={index % 2 === 0 ? "timeline-start" : "timeline-end"}
+            >
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div
+                className={
+                  index % 2 === 0
+                    ? "timeline-start"
+                    : "timeline-end md:text-end mb-10"
+                }
               >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className={index % 2 === 0 ? "timeline-start" : "timeline-end md:text-end mb-10"}>
-              <time className="font-mono italic">{exp.startDate.slice(0, 10)}</time>
-              <span> - </span>
-              <time className="font-mono italic">{exp.endDate.slice(0, 10)}</time>
-              <div className="text-lg font-black">{exp.title}</div>
-              {exp.description}
-            </div>
-            <hr />
-          </li>
-        ))}
-      </ul>
+                <time className="font-mono italic">
+                  {exp.startDate.slice(0, 10)}
+                </time>
+                <span> - </span>
+                <time className="font-mono italic">Continous Learning</time>
+                <div className="text-lg font-black">{exp.title}</div>
+                {exp.description}
+              </div>
+              <hr />
+            </li>
+          ))}
+        </ul>
+     
     </div>
   );
 };
 
-export default SectionWrapper(Work,"work");
+export default SectionWrapper(Work, "work");
--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-
+import { staggerContainer } from '../utils/motion'
 
 const SectionWrapper = (Component,idName) => 
 function HOC(){
     return (
     <motion.section 
+    variants={staggerContainer()}
     initial="hidden"
     whileInView="show"
     viewport={{once:true,amount:0.25}}
-    className=''
+    className='relative z-0'
     >
         <span className=" hash-span" id={idName}>
             &nbsp;
