a0a21d6202a13247cb4030810f7507984200b35a
mrcoder57 <tiwariji2300@gmail.com>
2024-03-21 16:21:45 +0530

    Fix routes for profiel

---
+++ src/pages/profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Hero from '../Components/Hero'
+import About from '../Components/About'
+import Work from "../Components/Work"
+import Projects from '../Components/Projects'
+import Contact from '../Components/Contact'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+const Profile = () => {
+  return (
+   <div>
+    {/* <Navbar/> */}
+    <Hero/>
+    <About/>
+    <Work/>
+    <Projects/>
+    <Contact/>
+    </div>
+  )
+}
+
+export default Profile
